34a041d412fe6796a837119b58cbf80c3479d531
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-01-20 22:58:23 -0700

    do exercise 3A

--- HaskellExercises03/src/Exercises03.hs
+++ HaskellExercises03/src/Exercises03.hs
@@ -30,10 +30,13 @@ macid = "TODO"
 -- NOTE if the input is less than 0, simply return 0
 -----------------------------------------------------------------------------------------------------------
 fib :: (Integral a) => a -> a
+
+
+
 fib n
-  | n <= 0 = error "TODO implement fib"
-  | n == 1 = error "TODO implement fib"
-  | otherwise = error "TODO implement fib"
+  | n <= 0 = 0
+  | n == 1 = 1
+  | otherwise =  fib (n-1) + fib (n-2)
 
 -- Exercise B
 -----------------------------------------------------------------------------------------------------------
